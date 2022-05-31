8e94e7f4bd753fbc386536e07d6d17104189a2ec
Sylvia-Krech <40416079+Sylvia-Krech@users.noreply.github.com>
2022-05-30 19:52:48 -0500

    [init] commit of script file

---
+++ autosolver.py
@@ -0,0 +1,20 @@
+import pyautogui
+import __autosolver
+
+if __name__ == "__main__":
+
+    ## main loop
+    loopFlag = True
+    while loopFlag:
+
+        ## grab screen
+
+        ## identify numbers + operators
+            # screenshot, then use locate on the screenshot, with pre-existing images of the 
+        ## solve
+
+        ## move
+
+        ## ensure is solved
+            # if it is, continue to next room
+            # otherwise reset
